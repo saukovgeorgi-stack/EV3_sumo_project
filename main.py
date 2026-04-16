3c187cdffd74f3c8f38f1b7b8cdc84005cad7ad7
Georgi <saukov.georgi@gmail.com>
2026-04-16 22:42:41 +0300

    Update motor control constants and methods

--- main.py
+++ main.py
@@ -15,17 +15,15 @@ us_left  = UltrasonicSensor(Port.S2)
 us_right = UltrasonicSensor(Port.S4)
 
 # константы
-MAX_POWER = 100        
-SEARCH_POWER = 70       
-DETECT_DIST_F = 400     #60 см
-DETECT_DIST_SIDE = 300  #450
+MAX_POWER = 1700        
+SEARCH_POWER = 900       
+DETECT_DIST_F = 700     #70 см
+DETECT_DIST_SIDE = 450  #45 см
 DOVOROT_MOTOR_DEG = 100 
 
 # Флаги состояния
 first_encounter = True
 last_turn_dir = 0  # 1 крутились вправо, -1 крутились влево, 0 стоим ровно
-
-wait(450)
 
 while True:
     dist_f = us_front.distance()
@@ -51,27 +49,27 @@ while True:
             first_encounter = False
 
         #атака
-        motor_l.dc(MAX_POWER)
-        motor_r.dc(MAX_POWER)
+        motor_l.run(MAX_POWER)
+        motor_r.run(MAX_POWER)
 
     #2 противник слева
     elif dist_l < DETECT_DIST_SIDE:
         last_turn_dir = -1
-        motor_l.dc(-MAX_POWER)
-        motor_r.dc(MAX_POWER)
+        motor_l.run(-MAX_POWER)
+        motor_r.run(MAX_POWER)
 
     #3 противник справа
     elif dist_r < DETECT_DIST_SIDE:
         last_turn_dir = 1
-        motor_l.dc(MAX_POWER)
-        motor_r.dc(-MAX_POWER)
+        motor_l.run(MAX_POWER)
+        motor_r.run(-MAX_POWER)
 
     #4 поиск
     else:
         if last_turn_dir == -1:
-            motor_l.dc(-SEARCH_POWER)
-            motor_r.dc(SEARCH_POWER)
+            motor_l.run(-SEARCH_POWER)
+            motor_r.run(SEARCH_POWER)
         else:
             last_turn_dir = 1
-            motor_l.dc(SEARCH_POWER)
-            motor_r.dc(-SEARCH_POWER)
+            motor_l.run(SEARCH_POWER)
+            motor_r.run(-SEARCH_POWER)
