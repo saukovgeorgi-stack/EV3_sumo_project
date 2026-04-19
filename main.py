8ca962f7abbb7f1ba85d10a811bbc71502a7a73e
Georgi <saukov.georgi@gmail.com>
2026-04-19 10:42:21 +0300

    Refactor sensor setup and improve motor control logic
    Updated sensor initialization and adjusted detection distances. Enhanced motor control logic for obstacle avoidance.

--- main.py
+++ main.py
@@ -4,53 +4,50 @@ from pybricks.ev3devices import Motor, UltrasonicSensor
 from pybricks.parameters import Port, Stop
 from pybricks.tools import wait
 
-# инициализация
+# Инициализация
 ev3 = EV3Brick()
 
 motor_r = Motor(Port.B)
 motor_l = Motor(Port.C)
 
 us_front = UltrasonicSensor(Port.S3)
-us_left  = UltrasonicSensor(Port.S2)
-us_right = UltrasonicSensor(Port.S4)
+us_left  = UltrasonicSensor(Port.S4) 
+us_right = UltrasonicSensor(Port.S2) 
 
 # константы
 MAX_POWER = 1700        
-SEARCH_POWER = 900       
-DETECT_DIST_F = 700     #70 см
-DETECT_DIST_SIDE = 450  #45 см
+SEARCH_POWER = 1100       
+DETECT_DIST_F = 700     # 70 см
+DETECT_DIST_SIDE = 550  # 55 см
 DOVOROT_MOTOR_DEG = 100 
 
 motor_l.control.limits(acceleration=10000) 
 motor_r.control.limits(acceleration=10000)
-# Флаги состояния
+
+# флаги состояния
 first_encounter = True
-last_turn_dir = 0  # 1 крутились вправо, -1 крутились влево, 0 стоим ровно
+last_turn_dir = 0  
 
 while True:
+    # 1.считываем только передний датчик 
     dist_f = us_front.distance()
-    dist_l = us_left.distance()
-    dist_r = us_right.distance()
 
-    #1 противник по центру
     if dist_f < DETECT_DIST_F:
         if first_encounter:
             motor_l.hold()
             motor_r.hold()
             wait(50)
-            # Если нас несло вправо, доворачиваем чуть влево, и наоборот
+            
             if last_turn_dir == 1:
-                # Доворот влево
-                motor_l.run_angle(1300, DOVOROT_MOTOR_DEG, wait=False)
-                motor_r.run_angle(1300, -DOVOROT_MOTOR_DEG, wait=True)
+                motor_l.run_angle(1400, DOVOROT_MOTOR_DEG, wait=False)
+                motor_r.run_angle(1400, -DOVOROT_MOTOR_DEG, wait=True)
             elif last_turn_dir == -1:
-                # Доворот вправо
-                motor_l.run_angle(1300, -DOVOROT_MOTOR_DEG, wait=False)
-                motor_r.run_angle(1300, DOVOROT_MOTOR_DEG, wait=True)
+                motor_l.run_angle(1400, -DOVOROT_MOTOR_DEG, wait=False)
+                motor_r.run_angle(1400, DOVOROT_MOTOR_DEG, wait=True)
             
             first_encounter = False
 
-        #атака
+        # аткуем
         if dist_f < 200:
             motor_l.dc(100)
             motor_r.dc(100)
@@ -58,24 +55,30 @@ while True:
             motor_l.run(MAX_POWER)
             motor_r.run(MAX_POWER)
 
-    #2 противник слева
-    elif dist_l < DETECT_DIST_SIDE:
-        last_turn_dir = -1
-        motor_l.run(-MAX_POWER)
-        motor_r.run(MAX_POWER)
-
-    #3 противник справа
-    elif dist_r < DETECT_DIST_SIDE:
-        last_turn_dir = 1
-        motor_l.run(MAX_POWER)
-        motor_r.run(-MAX_POWER)
-
-    #4 поиск
     else:
-        if last_turn_dir == -1:
-            motor_l.run(-SEARCH_POWER)
-            motor_r.run(SEARCH_POWER)
+        #2.противника спереди нет, считаем левый датчик.
+        dist_l = us_left.distance()
+        
+        if dist_l < DETECT_DIST_SIDE:
+            last_turn_dir = -1
+            motor_l.run(-MAX_POWER)
+            motor_r.run(MAX_POWER)
+        
         else:
-            last_turn_dir = 1
-            motor_l.run(SEARCH_POWER)
-            motor_r.run(-SEARCH_POWER)
+            # 3.противника слева нет, считаем правый датчик.
+            dist_r = us_right.distance()
+            
+            if dist_r < DETECT_DIST_SIDE:
+                last_turn_dir = 1
+                motor_l.run(MAX_POWER)
+                motor_r.run(-MAX_POWER)
+            
+            else:
+                # 4.вообще никого нет, поиск
+                if last_turn_dir == -1:
+                    motor_l.run(-SEARCH_POWER)
+                    motor_r.run(SEARCH_POWER)
+                else:
+                    last_turn_dir = 1
+                    motor_l.run(SEARCH_POWER)
+                    motor_r.run(-SEARCH_POWER)
